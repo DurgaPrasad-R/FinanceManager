21f7458db6dd08464040c0016ba77e58ef0d29a8
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2024-06-27 00:11:43 +0530

    chore: Update NavBar component to include navigation icons

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,10 +1,16 @@
 const NavBar = () => {
   return (
-    <div>
+    <div className="flex justify-between p-4">
       <div>
-        <img src="/logo.png" alt="logo" className="w-20" />
+        <img src="/logo.png" alt="logo" className="w-10 object-contain" />
       </div>
-      <div></div>
+      <div className="flex gap-5">
+        <img src="/home.png" alt="home" className="w-10 object-contain" />
+        <img src="/tranxn.png" alt="home" className="w-10 object-contain" />
+        <img src="/budget.png" alt="home" className="w-10 object-contain" />
+        <img src="/goal.png" alt="home" className="w-10 object-contain" />
+        <img src="/report.png" alt="home" className="w-10 object-contain" />
+      </div>
     </div>
   );
 };
